f00eec342fef823a61ea47512309f5b74b1b198a
hbbhsy <mss_hsy@hotmail.com>
2020-08-27 21:52:30 -0700

    "Day 5"

---
+++ LoopPractise/src/LoopPractice/LoopPractice.java
@@ -0,0 +1,42 @@
+package LoopPractice;
+
+public class LoopPractice {
+
+    public static void practiceWhileLoop() {
+        int x = 0;
+        while(x <5) {
+            System.out.println("The value of x is " + x);
+            // x = x+1
+            x++;
+        }
+    }
+
+    public static void practiceDoWhileLoop() {
+        int x = 0;
+        do {
+            System.out.println("The value of x is " + x);
+            x++;
+        } while (x <10);
+    }
+
+    public static void practiceForLoop() {
+        for (int x = 0; x < 3; x++) {
+            System.out.println("The value of x is " + x);
+//            if (x == 10) {
+//                break;
+//            }
+            for (int y =0; y < 3; y++) {
+                System.out.println("("+x+","+y+")");
+            }
+
+        }
+        System.out.println("This is the end");
+    }
+
+    public static void main(String[] args){
+        practiceForLoop();
+
+    }
+
+
+}
