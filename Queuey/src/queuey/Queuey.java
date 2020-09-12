247ada63b5eb71f4379930fea347639cc30e326e
hbbhsy <mss_hsy@hotmail.com>
2020-09-11 23:24:14 -0700

    Day 18

---
+++ Queuey/src/queuey/Queuey.java
@@ -0,0 +1,31 @@
+package queuey;
+
+import java.util.LinkedList;
+
+public class Queuey {
+
+    LinkedList queue;
+
+    // making a queue instance
+    public Queuey() {
+        queue = new LinkedList();
+    }
+
+    // Checking is the queque empty
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    // Check the size
+    public int size() {
+        return queue.size();
+    }
+
+    public void enqueue(int n) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
