24504c4f65631ad838dc906aef2d432cdafb76c9
hbbhsy <mss_hsy@hotmail.com>
2020-09-11 23:46:04 -0700

    Day 18

--- Queuey/src/queuey/Queuey.java
+++ Queuey/src/queuey/Queuey.java
@@ -1,6 +1,7 @@
 package queuey;
 
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class Queuey {
 
@@ -22,29 +23,41 @@ public class Queuey {
     }
 
     // Enqueueing an item (add an item)
-    public void enqueue(int n) {
-        queue.addLast(n);
+    public void enqueue(String s) {
+        queue.addLast(s);
     }
 
     // Dequeue the first item
-    public int dequeue() {
-        return (int) queue.remove();// Casting the dequeued item to an int type
+    public String dequeue() {
+        return (String) queue.remove(); // Casting the dequeued item to an string type
     }
 
     // Peek at the first item
-    public int peek() {
-        return (int) queue.get(0); // Get the first item and cast it to an int type
+    public String peek() {
+        return (String) queue.get(0); // Get the first item and cast it to an string type
     }
 
     public static void main(String[] args) {
+        System.out.println("Both queue and stack are buildin in Java, anc can be implemented using linkedlist.");
+        System.out.println();
+        System.out.println("Queue: First In First Out");
         Queuey q = new Queuey();
-        q.enqueue(5);
-        q.enqueue(7);
-        q.enqueue(6);
-        System.out.println("First out: "+q.dequeue());
-        System.out.println("Peek at second item: " + q.peek());
-        System.out.println("Second out: " + q.dequeue());
-        System.out.println("Third out: " + q.dequeue());
+        q.enqueue("hi");
+        q.enqueue("there");
+        System.out.print(q.dequeue() + " ");
+        System.out.println(q.dequeue()+ ".");
+
+        // Stack: Last in First out
+        System.out.println("Stack: Last In First Out");
+        Stack<String> stacky = new Stack<>(); // Good coding practice to add <String>
+        // Usually you want to work with only one data type in a linkedlist and good readability
+        stacky.push("there"); // Add an item to the top(last) of the stack
+        stacky.push("hi");
+        System.out.print(stacky.pop() + " "); // pop the top(last) item
+        System.out.println(stacky.pop() + "!");
+
+
+
 
 
 
