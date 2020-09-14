434d5fa9b8903e1becdf0577a1daa2520a5509a7
hbbhsy <mss_hsy@hotmail.com>
2020-09-13 23:15:57 -0700

    Day 19

---
+++ StarWarsInterface/src/starwarsinterface/Character.java
@@ -0,0 +1,9 @@
+package starwarsinterface;
+
+public interface Character {
+
+    public String base = "character";
+    public void attack();
+    public void heal();
+
+}
---
+++ StarWarsInterface/src/starwarsinterface/StarWarsInterface.java
@@ -0,0 +1,4 @@
+package starwarsinterface;
+
+public class StarWarsInterface {
+}
