8909626fc8f2a5a599eac89a9cef881f7b2158a9
hbbhsy <mss_hsy@hotmail.com>
2020-09-17 21:55:39 -0700

    Day 21

--- Generics/src/generics/Generics.java
+++ Generics/src/generics/Generics.java
@@ -1,6 +1,23 @@
 package generics;
 
 // Create a method to be able to find the maximum of two values
+// Here <E> denotes the type parameter of Node class .
+// The type parameter defines that it can refer to any type (like String, Integer, Employee etc.).
+// Java generics have type parameter naming conventions like following:
+// T - Type
+// E - Element
+// K - Key
+// N - Number
+// V - Value
+// D - Data
+// S,U,V etc. - 2nd, 3rd, 4th types
+// By convention, type parameter names are single, uppercase letters.
+// This stands in sharp contrast to the variable naming conventions
+// that you already know about, and with good reason: Without this convention,
+// it would be difficult to tell the difference between a type variable and an ordinary
+// class or interface name. The most commonly used type parameter names are:
+
+
 public class Generics implements Comparable{
 
     public static <T extends Comparable<T>> T findMax(T a, T b) {
