378e0775ed97c42d02e7316df77ac577ed5bc0b3
hbbhsy <mss_hsy@hotmail.com>
2020-09-23 23:14:05 -0700

    Day 24

---
+++ HangmanApp/src/hangmanapp/HangmanApp.java
@@ -0,0 +1,10 @@
+package hangmanapp;
+
+import java.util.Scanner;
+
+public class HangmanApp {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
