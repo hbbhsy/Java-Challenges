17e19598df5143f6b782397fcea39b06c42b82bf
hbbhsy <mss_hsy@hotmail.com>
2020-09-24 22:31:09 -0700

    Day 24

--- HangmanApp/src/hangmanapp/HangmanApp.java
+++ HangmanApp/src/hangmanapp/HangmanApp.java
@@ -6,5 +6,15 @@ public class HangmanApp {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to hangman!");
+        System.out.println();
+        System.out.println("Rule: blablabla.");
+
+        boolean doYouWantToPlay = true;
+        while (doYouWantToPlay) {
+            // keep playing
+            System.out.println();
+
+        }
     }
 }
