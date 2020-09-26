38a960b0c89e56204b7f13f8cc6739f420d7c261
hbbhsy <mss_hsy@hotmail.com>
2020-09-25 21:38:50 -0700

    Day 24

---
+++ HangmanApp/src/hangmanapp/Hangman.java
@@ -0,0 +1,7 @@
+package hangmanapp;
+
+public class Hangman {
+
+
+
+}
--- HangmanApp/src/hangmanapp/HangmanApp.java
+++ HangmanApp/src/hangmanapp/HangmanApp.java
@@ -13,7 +13,15 @@ public class HangmanApp {
         boolean doYouWantToPlay = true;
         while (doYouWantToPlay) {
             // keep playing
+            while (!game.gameOver()) {
+
+            }
+
+            // Play or not?
             System.out.println();
+            System.out.println("Do you want to play another game? Enter Y/N");
+            Character response = (sc.next().toUpperCase()).charAt(0);
+            doYouWantToPlay = (response == 'Y');
 
         }
     }
