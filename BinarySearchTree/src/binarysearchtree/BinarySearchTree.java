329448adbdd6bcef5e4b50362e73d1022aa396be
hbbhsy <mss_hsy@hotmail.com>
2020-10-07 00:28:30 -0700

    Day 26

--- BinarySearchTree/src/binarysearchtree/BinarySearchTree.java
+++ BinarySearchTree/src/binarysearchtree/BinarySearchTree.java
@@ -2,7 +2,7 @@ package binarysearchtree;
 
 public class BinarySearchTree {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         EmptyBST e = new EmptyBST();
         NonEmptyBST n = new NonEmptyBST(5);
         Testers.checkIsEmpty(e);
--- BinarySearchTree/src/binarysearchtree/Testers.java
+++ BinarySearchTree/src/binarysearchtree/Testers.java
@@ -1,16 +1,16 @@
 package binarysearchtree;
 
-public class Testers {
+public class Testers  {
 
-    public static void checkIsEmpty(Tree t) {
+    public static void checkIsEmpty(Tree t) throws Exception {
         // if the tree t is an instance of EmptyBST --> isEmpty --> true
         // if the tree is an instance of NonEmptyBST --> t.isEmpty --> false
         if (t instanceof EmptyBST) {
-            if (t.isEmpty()) {
-                System.out.println("All is good, the tree is an EmptyBST and it's empty.");
+            if (!t.isEmpty()) {
+                throw new Exception("All is not good, the tree is an EmptyBST and it's not empty");
             } else if (t instanceof NonEmptyBST) {
                 if (!t.isEmpty()) {
-                    System.out.println("All is good, the tree is an NonEmptyBST and it's not empty.");
+                    throw new Exception("All is not good, the tree is an NonEmptyBST and it's empty.");
                 }
             }
         }
