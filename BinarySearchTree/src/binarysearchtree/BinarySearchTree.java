a3e0fc7eb7c1d0a7e328bb3a78dd66aececfb92c
hbbhsy <mss_hsy@hotmail.com>
2020-10-09 00:14:23 -0700

    Day 27

--- BinarySearchTree/src/binarysearchtree/BinarySearchTree.java
+++ BinarySearchTree/src/binarysearchtree/BinarySearchTree.java
@@ -5,8 +5,22 @@ public class BinarySearchTree {
     public static void main(String[] args) throws Exception {
         EmptyBST e = new EmptyBST();
         NonEmptyBST n = new NonEmptyBST(5);
-        Testers.checkIsEmpty(e);
-        Testers.checkIsEmpty(n);
+//        Testers.checkIsEmpty(e);
+//        Testers.checkIsEmpty(n);
+
+//        Testers.checkAddMemberCardinality(e, 5);
+//        Testers.checkAddMemberCardinality(n, 5);
+//        Testers.checkAddMemberCardinality(n, 6);
+
+        for (int i = 0; i < 1000; i++) {
+            Tree t;
+            if (i % 10 == 0) {
+                t = Testers.rndTree(0);
+            } else {
+                t = Testers.rndTree(10);
+            }
+            Testers.checkAddMemberCardinality(t, i);
+        }
 
     }
 }
--- BinarySearchTree/src/binarysearchtree/Testers.java
+++ BinarySearchTree/src/binarysearchtree/Testers.java
@@ -1,6 +1,24 @@
 package binarysearchtree;
 
+import java.util.Random;
+
 public class Testers  {
+
+    // random integers
+    public static int rndInt(int min, int max) {
+        Random rand = new Random();
+        return rand.nextInt((max - min) + 1) + min;
+    }
+
+    // random binary search trees
+    public static Tree rndTree(int count) {
+        if (count == 0) {
+            return new EmptyBST();
+        } else {
+            return rndTree(count - 1).add(rndInt(0, 50));
+        }
+    }
+
 
     public static void checkIsEmpty(Tree t) throws Exception {
         // if the tree t is an instance of EmptyBST --> isEmpty --> true
@@ -22,9 +40,13 @@ public class Testers  {
             if (t.member(x)) {
                 throw new Exception("The cardinality increased by 1., but the thing"
                                     + " that was added was already a member of the tree.");
+            } else {
+                System.out.println("Good");
             }
         } else if (nT == t.cardinality()) {
             throw new Exception("Something is wrong with our program.");
+        } else {
+            System.out.println("Good.");
         }
 
     }
